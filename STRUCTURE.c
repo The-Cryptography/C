09297e8e8b1d7a2bcbfd05f5b3015c3c0988b53b
Anviksha Jain <56529970+Anvikshajain@users.noreply.github.com>
2020-08-14 09:23:57 +0530

    Update STRUCTURE.c

--- STRUCTURE.c
+++ STRUCTURE.c
@@ -1,1 +1,26 @@
+/*
+@ file name
+@ code by author_name
+@ two line description of the code
+*/
 
+#include<stdio.h>
+#include<conio.h>
+
+//doing sum of two integers
+void do_sum(int a,int b)
+{
+  int c=a+b;
+  printf("Sum of numbers is : %d",c);
+}
+
+int main()
+{
+  //passing values to function for 
+  //adding them
+  do_sum(5,8);
+  return 0;
+}
+
+//comment down every required line for better 
+// understanding of code
